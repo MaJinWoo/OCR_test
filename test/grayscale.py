e52657d045f217c0ab6228c40dbbd31978cf8824
MaJinWoo <maddonge@naver.com>
2022-10-13 14:26:41 +0900

    ocr test (grayscale)

--- test/grayscale.py
+++ test/grayscale.py
@@ -1,13 +1,18 @@
 import cv2
 import pytesseract
 
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 path = 'pic/lotto.png'
-
 image = cv2.imread(path)
 text = pytesseract.image_to_string(image, lang='kor+eng')
 print('Texto: ', text)
-
 cv2.imshow('Image', image)
+
+img_gray = cv2.imread(path, cv2.COLOR_BGR2GRAY)
+img_gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+cv2.imshow("grayscale", img_gray)
+
+text_gray = pytesseract.image_to_string(img_gray, lang='kor+eng')
+print(text_gray)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
